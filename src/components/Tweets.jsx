43331570a38b24483462694dfd05d6d88ea65fb9
Théodorebinda <theodorebinda@gmail.com>
2023-11-24 15:52:17 +0100

    integration of the autoDetail value

--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -2,51 +2,94 @@ import React from "react";
 import profil from "../images/tweet-profile-photo.png";
 import tweetProfil from "../images/Tweet-Profile-Photo.svg";
 import tweetImage from "../images/tweet-image.png";
+import certified from "../icones/icones/Group.png";
 import comment from "../icones/actions/Reply.png";
-import react from "../icones/actions/React.png";
+import reaction from "../icones/actions/React.png";
 import retweet from "../icones/actions/Retweet.png";
 import share from "../icones/actions/Buton.png"
 
 
 const tweets = [
     {autor : "CNN",
+    isCertified : certified,
+    autorDetails : "@CNN. 7m",
      tweetAvatar : profil,
      text : "President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a 'major breakthrough' that would serve to both strengthen the US steel industry and combat the global climate crisis",
      image: null,
      actions:
+     [
         {
-            comment:comment,
-            like:react,
-            retweet:retweet,
-            share:share
+            label:comment,
+            nbre: 123
+         },
+         {
+            label:retweet,
+            nbre: 234
+         },
+         {
+            label:reaction,
+            nbre:315
+         },
+         {
+            label:share,
+            nbre:456
          }
+        ]
      
     
     },
     {  autor : "The New York Times",
+        isCertified : certified,
+        autorDetails : "@nytimes. 2h",
         tweetAvatar : tweetProfil,
         text : "President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a 'major breakthrough' that would serve to both strengthen the US steel industry and combat the global climate crisis",
         image : tweetImage,
         actions:
+        [
             {
-                comment:comment,
-                like:react,
-                retweet:retweet,
-                share:share
+                label:comment,
+                nbre: 123
+             },
+             {
+                label:retweet,
+                nbre: 234
+             },
+             {
+                label:reaction,
+                nbre:315
+             },
+             {
+                label:share,
+                nbre:false
              }
+            ]
          
     },
     { autor : "Twitter",
-    tweetAvatar : tweetProfil,
-    text : "BIG New lol jk still Twitter",
-    image : null,
-    actions:
-        {
-            comment:comment,
-            like:react,
-            retweet:retweet,
-            share:share
-         }
+        isCertified : certified,
+        autorDetails : "@twitter. Oct 29",
+        tweetAvatar : tweetProfil,
+        text : "BIG New lol jk still Twitter",
+        image : null,
+        actions:
+        [
+            {
+                label:comment,
+                nbre: 123
+             },
+             {
+                label:retweet,
+                nbre: 234
+             },
+             {
+                label:reaction,
+                nbre:315
+             },
+             {
+                label:share,
+                nbre:false
+             }
+            ]
      
     }
 ]
@@ -58,12 +101,28 @@ function Tweets (){
             </dir>
             <div className="tweet-content">
                 <div className="tweet-body">
-                    <strong className="tweet-title-autor">{tweet.autor}</strong> 
-                    <p className="tweet-text">{tweet.text}</p>
-                    <img className="tweet-image" src={tweet.image} alt="" />
+                    <div className="tweet-title">
+                        <strong className="tweet-title-autor">{tweet.autor}</strong> 
+                        <img  src={tweet.isCertified} alt="" />
+                        <p className="tweet-title-details">{tweet.autorDetails}</p>
+                    </div>
+                    <div className="tweet-text">
+                        <p >{tweet.text}</p>
+                    </div>
+                    <div className="tweet-image">
+                        <img  src={tweet.image} alt="" />
+                    </div>
                 </div>
                 <div className="tweet-actions">
-                <img className="tweet-action" src={tweet.actions} alt="" />
+                    {tweet.actions.map(({label,nbre}) =>
+                    <div className="tweet-action"> 
+                        <img  alt="" src={label}/>
+                        <div>{nbre}</div>
+
+                    </div>
+                     )}
+
+
                 </div>
             </div>
             
