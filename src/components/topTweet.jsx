4091d38e1d0540710192ce518fc9782e60a4084f
Theodorebinda <theodorebinda@gmail.com>
2023-12-07 14:37:48 +0000

    refactor

--- src/components/topTweet.jsx
+++
@@ -1,8 +0,0 @@
-import TopTweet from "../icones/icones/Timeline-Prop.png"
-// Composant qui gere le btn TopTweet dans Header
-function TopTweets(){
-    return (
-        <img className="top-tweet" src={TopTweet} alt="" />
-        )
-}
-export default TopTweets
---
+++ src/utiles/butonMetas.js
@@ -0,0 +1,8 @@
+import emoji from "../icones/icones/Emoji.png"
+import schedule from "../icones/icones/Schedule.png"
+import gif from "../icones/icones/Gif.png"
+import poll from "../icones/icones/Poll.png"
+import media from "../icones/icones/img.png"
+
+const butonMetas =[media,gif,poll,emoji,schedule];
+export default butonMetas;
